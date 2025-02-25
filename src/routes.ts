043a5d00fe4ed7c6ee464fce42d0204b38533864
Tigran Mesropyan <tikodeveloper@gmail.com>
2025-02-25 15:03:03 +0400

    fix:

--- src/routes.ts
+++ src/routes.ts
@@ -34,7 +34,7 @@ export default (knex: Knex): ServerRoute[] => {
                         name: Joi.string().min(2).max(100).required(),
                         email: Joi.string().email().required(),
                         password: Joi.string().min(8).max(30).required(),
-                        password_repeat: Joi.valid(Joi.ref("password"))
+                        password_confirm: Joi.valid(Joi.ref("password"))
                             .required()
                             .messages({
                                 "any.only": "Passwords do not match",
